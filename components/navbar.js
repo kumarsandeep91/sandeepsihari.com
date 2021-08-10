5f00f89507ac88a2b98c5944c8a7edcb3bc875ed
Sandeep Sihari <kumarsandeep91[at]gmail.com>
2021-08-11 00:35:36 +0530

    navbar

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="surface-16 padding-hr-1 top-nav">
+      <a href="/">
+        <spna>&lt;Techno</spna> &nbsp; <span>Baba &#47;&gt;</span>
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
