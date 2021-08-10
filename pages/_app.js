22dd143ba5f4466293f5ffe8e6135b8ee281ebc1
Sandeep Sihari <kumarsandeep91[at]gmail.com>
2021-08-11 00:14:29 +0530

    few more global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import Navbar from '../components/navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header><Navbar /></header>
+      <Component {...pageProps} />
+      <footer></footer>
+    </>
+  );
 }
 
 export default MyApp
