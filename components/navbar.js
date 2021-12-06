1ba3151e9975a53ec8a0c1367247509d63f70c97
Sandeep Sihari <kumarsandeep91@gmail.com>
2021-12-06 20:17:39 +0530

    favicon and navbar update

--- components/navbar.js
+++ components/navbar.js
@@ -1,18 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable @next/next/no-html-link-for-pages */
 import React, { useState, useEffect } from "react";
+import { DEVICE_SIZES } from "../constants";
+import { GithubIcon, HamburgerIcon } from "../svgs";
 
 export const Logo = () => {
 	return (
 		<a href="/" className="logo">
 			<img
 				srcSet="
-      /logo-sm.png 576w,
-      /logo-md.png 768w,
-      /logo-lg.png 992w,
-      /logo-xl.png 1200w,
-      /logo-xxl.png 1400w,
-    "
+					/logo-sm.png 576w,
+					/logo-md.png 768w,
+					/logo-lg.png 992w,
+					/logo-xl.png 1200w,
+					/logo-xxl.png 1400w,
+					"
 				src="/logo-sm.png"
 				alt="Logo"
 			/>
@@ -20,31 +22,11 @@ export const Logo = () => {
 	);
 };
 
-export const Github = () => {
+export const Github = (props) => {
 	return (
 		<a href="https://github.com/kumarsandeep91" alt="Github">
-			<img
-				srcSet="
-      /social/Icon_github-sm.png 576w,
-      /social/Icon_github-md.png 768w,
-      /social/Icon_github-lg.png 992w,
-      /social/Icon_github-xl.png 1200w,
-      /social/Icon_github-xxl.png 1400w,
-    "
-				src="/social/Icon_github-sm.png"
-				alt="Github Icon"
-			/>
+			<GithubIcon {...props} />
 		</a>
-	);
-};
-
-export const Hamburger = () => {
-	return (
-		<img
-			className="hamburger"
-			src="/icons/Icon_hamburger.svg"
-			alt="More options"
-		/>
 	);
 };
 
@@ -55,7 +37,7 @@ const Navbar = () => {
 		setWindowWidth(window.innerWidth);
 	}, []);
 
-	if (windowWidth >= 768) {
+	if (windowWidth >= DEVICE_SIZES.md) {
 		return (
 			<nav className="top-nav flex center">
 				<div className="content flex-full-width space-between-inline center-block">
@@ -76,16 +58,16 @@ const Navbar = () => {
 						</li>
 					</ul>
 
-					<Github />
+					<Github className="icon" />
 				</div>
 			</nav>
 		);
 	} else {
 		return (
 			<nav className="top-nav flex-full-width space-between-inline center-block">
-				<Hamburger />
+				<HamburgerIcon className="icon" />
 				<Logo />
-				<Github />
+				<Github className="icon" />
 			</nav>
 		);
 	}
--- constants.js
+++ constants.js
@@ -11,4 +11,25 @@ export const SKILLS = [
 	"Redux",
 ];
 
+export const DEVICE_SIZES = {
+	xs: 1,
+	sm: 576,
+	md: 768,
+	lg: 992,
+	xl: 1200,
+	xxl: 1400,
+};
+export const MEDIA = {
+	xs: `@media (min-width: ${DEVICE_SIZES.xs}px)`,
+	sm: `@media (min-width: ${DEVICE_SIZES.sm}px)`,
+	md: `@media (min-width: ${DEVICE_SIZES.md}px)`,
+	lg: `@media (min-width: ${DEVICE_SIZES.lg}px)`,
+	xl: `@media (min-width: ${DEVICE_SIZES.xl}px)`,
+	xxl: `@media (min-width: ${DEVICE_SIZES.xxl}px)`,
+};
+
+export const MAX_CONTENT_WIDTH = 1200;
+
+export const MAX_NAVBAR_WIDTH = 1350;
+
 export default {};
